d112b60e2a0ab24b8401cf78253da0231a48ac6e
linsong <910564058@qq.com>
2017-08-27 21:35:20 +0800

    大部分数据

---
+++ app/src/main/java/com/example/coolweather/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.example.coolweather.gson;
+
+/**
+ * Created by Administrator on 2017/8/27.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
